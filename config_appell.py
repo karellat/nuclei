c8d8f0156b01276a664a8a8ad3534ebb38e23a1e
Tomas Karella <karellt@utia.cas.cz>
2022-07-15 16:38:25 +0200

    ZI config

--- config_appell.py
+++ config_appell.py
@@ -1,14 +1,14 @@
 from appell_polynomials_3D import Appell_Type, Appell_polynomial_weights
-from invariant3d import ZernikeInvariants3D, AppellInvariant3D, GaussHermiteInvariants3D
+from invariant3d import AppellInvariant3D, GaussHermiteInvariants3D
 
 # Basic params
 SPHERE_RADIUS = 20
 BATCH_SIZE = 5
 MAX_RANK = 6
 # First
-#MASK_IDS = [107, 156, 204, 253, 302, 351, 399, 448, 497, 58]
+MASK_IDS = [107, 156, 204, 253, 302, 351, 399, 448, 497, 58]
 # Second
-MASK_IDS = [10, 129, 188, 248, 307, 367, 426, 486, 546, 69]
+#MASK_IDS = [10, 129, 188, 248, 307, 367, 426, 486, 546, 69]
 CM = "IMAGE" # MASK - center of mask calculated from image or mask
 PATH = "data/c_elegans_nuclei/train/images/C18G1_2L1_1.tif"
 PATH_MASK = "data/c_elegans_nuclei/train/masks/C18G1_2L1_1.tif"
@@ -19,10 +19,13 @@ SKIP_ZEROS = True
 INVARIANTS_NUM = 77
 SRZ = SPHERE_RADIUS * 2 + 1
 # Model
-model_type = ZernikeInvariants3D
+model_type = AppellInvariant3D
 # Parameters
 model_params = dict(
-    types=0,
+    appell_type=Appell_Type.U,
+    appell_weight=Appell_polynomial_weights.WEIGHTED_5,
+    appell_parameter_s=1,
+    appell_type_s=0,
     num_invariants=INVARIANTS_NUM,
     cube_side=SRZ,
     max_rank=MAX_RANK,
